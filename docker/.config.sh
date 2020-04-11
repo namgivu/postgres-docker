136f805d2c35b6f28e0d3b75de534a89d699f95c
namgivu <namgivu@gmail.com>
2020-04-11 12:48:15 +0700

    STACK_ID replace ENV

--- docker/.config.sh
+++ docker/.config.sh
@@ -2,18 +2,16 @@
 cSH=`cd $(dirname $BASH_SOURCE) && pwd`  # get cSH=config_SCRIPT_HOME executed script's path, containing folder, cd & pwd to get container path
 cAH=`cd $cSH/.. && pwd`
 
-# create .env from .env00.$ENV
-if [[ -z $ENV ]]; then echo 'System variable $ENV not defined'; exit 1; fi
-    create_from_f="$cAH/config_vault/.env00.$ENV"; if [[ ! -f $create_from_f ]]; then echo "File .env00 for :$ENV not found in $cAH/config_vault/"; exit 1; fi
+# create .env from .env00.$STACK_ID
+if [[ -z $STACK_ID ]]; then echo 'System variable $STACK_ID not defined'; exit 1; fi
+    create_from_f="$cAH/config_vault/.env00.$STACK_ID"; if [[ ! -f $create_from_f ]]; then echo "File .env00 for :$STACK_ID not found in $cAH/config_vault/"; exit 1; fi
           env00_f="$cSH/.env00"  # xxx_f aka xxx_file
             env_f="$cSH/.env"
 
     cp -f $create_from_f $env00_f
 
     source $env00_f
-    if [[ -z $STACK_ID ]]; then echo "Param :STACK_ID not found in $env00_f"; exit 1; fi
-        export STACK_ID="${STACK_ID}"
-            template_str=`cat $env00_f`; eval "echo \"$template_str\" " > $env_f  # bash to fill in template ref. https://unix.stackexchange.com/a/560408/17671
+        template_str=`cat $env00_f`; eval "echo \"$template_str\" " > $env_f  # bash to fill in template ref. https://unix.stackexchange.com/a/560408/17671
 
 # load .env
 if [[ ! -f "$cSH/.env" ]]; then echo "File not found .env at $cSH - please clone one from *.env in `$cSH/../config_vault` "; exit 1; fi
