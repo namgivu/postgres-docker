f7d0d7b9d1c1522a117b079cff9219230f321a16
namgivu <namgivu@gmail.com>
2020-04-11 12:24:08 +0700

    add aftermath

--- docker/summary.sh
+++ docker/summary.sh
@@ -3,7 +3,16 @@ SH=`cd $(dirname $BASH_SOURCE) && pwd`  # get SH=executed script's path, contain
 
 source "$SH/.config.sh"
 
+echo "--> AFTERMATH"
+echo
+docker ps | grep -E "$CONTAINER_NAME|$EXPOSED_PORT" --color=always
+
+echo
+echo $STACK_ID
+docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c 'select version()' -t  | grep -iE "PostgreSQL [^(]+" --color=always
+#                               #print postgres version of the container           #print postgres version
+
 echo "
-** sample usage **
+--> SAMPLE USAGE
 docker exec -it $CONTAINER_NAME psql -U $POSTGRES_USER -c 'select version()' -t
 "
