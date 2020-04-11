62f76449bf02f5c75e32fca3ee0fda3633a386ff
namgivu <namgivu@gmail.com>
2020-04-11 12:46:15 +0700

    progress text

--- docker/down.sh
+++ docker/down.sh
@@ -4,6 +4,8 @@ SH=`cd $(dirname $BASH_SOURCE) && pwd`  # get SH=executed script's path, contain
 source "$SH/.config.sh"
 
 cd $SH  # must run at this script home folder to load .env
-    docker stop $CONTAINER_NAME
-    docker rm   $CONTAINER_NAME
+    echo 'Dropping container...'
+        docker stop $CONTAINER_NAME
+        docker rm   $CONTAINER_NAME
+    echo 'Dropping container... DONE'
 cd - 1>/dev/null
