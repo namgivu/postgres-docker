5f13aa7e83fd2102a658e92e3a6f68f00f53e9a5
namgivu <namgivu@gmail.com>
2020-04-14 18:09:48 +0700

    rename step

--- README.md
+++ README.md
@@ -3,9 +3,8 @@ Log in postgres using user+pass or using peer login with host/ip+OS-user
 ref. https://hub.docker.com/_/postgres
 
 `00`  simplest docker run that work - default postgres user and database to be postgres
-
-`00b` try all kind of os user
+`00b` dropping -e POSTGRES_PASSWORD will make it fail
+`00c` try all kind of os user
       we aim at running with no user and no password
       
-`01`  dropping -e POSTGRES_PASSWORD will make it fail
-`02` 
+`01`  custom user+pass for psql
--- bin/00b.sh
+++ bin/00b.sh
@@ -4,16 +4,9 @@ docker stop -f ccc 1>/dev/null 2>&1
 docker rm   -f ccc 1>/dev/null 2>&1
 
 
-docker run  -e POSTGRES_PASSWORD=some-pass  --name ccc       -d                     postgres:11
-#           #set password                   #container name  #daemon ie background  #image name
+docker run  --name ccc       -d                     postgres:11
+#           #container name  #daemon ie background  #image name
 
 echo "Booting ..."; sleep 1  # give some time for the container to finish booting
-
-echo; docker exec              ccc  bash -c "psql             -c \" select '00_0' \"  "
-echo; docker exec -u root      ccc  bash -c "psql             -c \" select '00_1' \"  "
-echo; docker exec -u root      ccc  bash -c "psql -U postgres -c \" select '00_2' \"  "
-echo; docker exec -u postgres  ccc  bash -c "psql             -c \" select '00_3' \"  "
-
-echo; docker exec -u postgres  ccc  bash -c "psql             -c \" select '00_4 psql with no user+pass' _ \"  -t "
-#                 #set os user when bash with docker exec
-#                 #ref. https://docs.docker.com/engine/reference/commandline/exec/
+echo
+docker exec ccc  bash -c "psql -Upostgres -c 'select 1' "
