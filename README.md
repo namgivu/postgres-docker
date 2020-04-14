36d56262e4920797905b38a7ac27940489872fc3
namgivu <namgivu@gmail.com>
2020-04-14 17:30:28 +0700

    00 n 01 run

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # postgres-auth-start
 Log in postgres using user+pass or using peer login with host/ip+OS-user
+ref. https://hub.docker.com/_/postgres
+
+`00` simplest docker run that work
+`01` dropping -e POSTGRES_PASSWORD will make it fail
---
+++ bin/01.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+docker stop -f ccc 1>/dev/null 2>&1
+docker rm   -f ccc 1>/dev/null 2>&1
+
+
+docker run  --name ccc       -d                     postgres:11
+#           #container name  #daemon ie background  #image name
+
+echo "Booting ..."; sleep 2  # give some time for the container to finish booting
+echo
+docker exec ccc  bash -c "psql -Upostgres -c 'select 1' "
