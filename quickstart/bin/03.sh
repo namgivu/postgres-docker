cfbbdaedb58fc09c8014382cfc3802571f0c19db
namgivu <namgivu@gmail.com>
2020-05-12 09:52:27 +0700

    quickstart WIP

--- quickstart/bin/03.sh
+++ quickstart/bin/03.sh
@@ -19,3 +19,16 @@ echo; docker exec -u $USER   ccc bash -c "psql ddd -c \" select 1 \" "
 #                 #os user                     #db
 
 echo; psql -p 20414 ddd -c 'select 1'
+
+
+# wire up another_machine :am, to prove that can psql connect as long as having same user with :ddd
+docker stop -f 'another_machine' 1>/dev/null 2>&1
+docker rm   -f 'another_machine' 1>/dev/null 2>&1
+    echo; docker run --net host                                   --name 'another_machine'  -e POSTGRES_PASSWORD='am-pass'  -d postgres
+    #                #note we use :host network here on purpose
+    #                #so that the new machine can see :ddd as localhost:20414
+        sleep 2
+        docker exec 'another_machine' useradd $USER
+        echo; docker exec -u $USER  'another_machine'  bash -c "psql -h localhost                   -p 20414                ddd -c \" select 1 \" "
+        #                                                            #note must use localhost here  #note this is ccc port
+
